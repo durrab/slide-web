0da6bf274fdcfe8a7b1c8cc7c628fe9c0398e35f
Durrab Jami Khan <durrab@gmail.com>
2013-01-27 16:15:50 +0500

    Add New Deck using AMD and Backbone
    Now you can Add number of Slides to the Deck -
    1. There is a Slide Button which will be used to add number of slides.
    So Type the Slide title and then press add slide button
    2. Once done then add the ID, Title and Author and press Deck
    3. It will directly takes you to the New Deck Slides.
    
    Note: There is a problem with collection - if you refresh the page then
    the created decks will be removed because collection is getting
    populated and reset every time from decks collection

--- public/js-amd/view/newdeck.js
+++ public/js-amd/view/newdeck.js
@@ -2,7 +2,8 @@ define(function(require) {
 	var Backbone = require('backbone');
 	var template = require('hbs!../../tmpl/newdeck');
 	var DeckView = require('view/deck');
-	var Deck = require('model/Deck')
+	var Deck = require('model/Deck');
+	var Slide = require('model/Slide');
 	var View =  Backbone.View.extend({
 	  template: template,
 	  collection: null,
@@ -13,8 +14,16 @@ define(function(require) {
 	  this.render();
 	  
 	  $("#add_button").click(_.bind(this.onAddButtonClick,this));
-	  
+	  $("#add_slide").click(_.bind(this.onAddSlideButtonClick,this));
         },
+        onAddSlideButtonClick: function(event){
+                var list = $("#slideList");
+                list.append("<li>"+$("#slide_input").val()+"</li>");
+              
+              
+              
+        }
+        ,
         onAddButtonClick: function(event){
         	
         var deckTitle = $("#title_input").val();
@@ -48,11 +57,23 @@ define(function(require) {
 			  	        var newId = deck.get("id");
 			  	        var newTitle = deck.get("title");
 			  	        var newAuthor = deck.get("author");
-			  	        var newDeck = new Deck({id:newId, title:newTitle, author:newAuthor,slides: [{title:"Durrab Web Deck"}, {title: "Durrab Mobile!"}, {title: "Durrab End!"}]});
+			  	        
+			  	        var list = $("#slideList");
+                			var listItems = list.children();
+                			var deckSlides = [listItems.length];
+                			
+                			$.each(
+					listItems,
+					function( index, objValue ){
+					 
+					    var slide = new Slide({title: objValue.innerHTML});
+					    deckSlides[index] = slide;
+					
+					});
+			  	        
+			  	        var newDeck = new Deck({id:newId, title:newTitle, author:newAuthor,slides: deckSlides});
 			  	        this.collection.add(newDeck);
 			  	        Backbone.history.navigate('decks/'+deck.get("id"), true); 
-			  	        //router.navigate('decks/'+deck.get(id), true);
-			  		//alert(deck.get('id'));
 			  	 },this),
 			  	 error: function(){
 				        alert("Error Occured: " + reply.statusText);
